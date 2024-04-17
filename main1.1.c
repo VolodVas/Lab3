33ef7eff01665cfe31d6ad3fa1e10ce7c805f22f
Volodymyr Tokar <160426346+VolodVas@users.noreply.github.com>
2024-04-17 21:49:06 +0300

    Add files via upload

---
+++ main1.1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(void) {
+    int x, z;
+  float result;
+  
+printf("Будь-ласка введіть x:");
+  scanf("%d", &x);
+  printf("Будь-ласка введіть z:");
+    scanf("%d", &z);
+
+  if (x == 5) {
+    result = 3 + x * z;
+    printf("%s %.1f" , "Відповідь:", result);
+    return 0;
+  }
+    else if (x == -5) {
+    result = (float)(x + z) / (float)(7 - z);
+    printf("%s %.1f" , "Відповідь:", result);
+    return 0;
+    }
+      else if (x == 0) {
+    result = x;
+    printf("%s %.1f" , "Відповідь:", result);
+    return 0;
+      }
+        else (x != 5 && x != -5 && x != 0);
+        {
+    printf("Неможливо обчислити");
+    return 0;
+  }
+  return 0;
+}
